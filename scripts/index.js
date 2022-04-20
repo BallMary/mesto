4791015a852f89ec5aa52df63d0f2b22e34dad3e
Mary Ball <marusaball@yandex.ru>
2022-04-21 00:02:22 +0300

    Проектная работа 6

--- scripts/index.js
+++ scripts/index.js
@@ -48,7 +48,15 @@ const popupOpenPhoto = document.querySelector('.popup_open-photo');
 const closeButtonEdit = popupEdit.querySelector('.popup__close-button');
 const closeButtonCard = popupCard.querySelector('.popup__close-button');
 const closeButtonPhoto = popupOpenPhoto.querySelector('.popup__close-button');
+const popupButtonCard = popupCard.querySelector('.popup__button_create');
+const popupButtonEdit = popupEdit.querySelector('.popup__button_save');
+const titleCardError = popupCard.querySelector('#title-card-error');
+const linkError = popupCard.querySelector('#link-error');
+const nameError = popupEdit.querySelector('#name-error');
+const aboutError = popupEdit.querySelector('#about-error');
 
+
+// функция создания карточки
 const createCards = (photoName, photoLink) => {
   const template = document.querySelector('#template');
   const place = template.content.querySelector('.element').cloneNode(true);
@@ -78,10 +86,12 @@ const renderCards = (photoName, photoLink) => {
 }
 
 const addCards = (evt) => {
+  
   evt.preventDefault();
   const photoName =  titleInputCard.value;
   const photoLink = linkInputCard.value;
   renderCards(photoName, photoLink);
+
   closePopup(popupCard);
   formElementCard.reset();
 }
@@ -94,18 +104,30 @@ const elements = initialCards.map(function(item) {
 elementsContainer.append(...elements);
 formElementCard.addEventListener('submit', addCards);
 
+
 closeButtonEdit.addEventListener('click', () => closePopup(popupEdit));
 closeButtonCard.addEventListener('click', () => closePopup(popupCard));
+  // formElementCard.reset();
+
+  
 closeButtonPhoto.addEventListener('click', () => closePopup(popupOpenPhoto));
 // работа попапов
 
+// функция открытия попапа редактирования профиля
 function editPopup() {
-  openPopup(popupEdit);
   nameInputEdit.value = profileTitle.textContent;
   jobInputEdit.value = profileSubtitle.textContent;
+  popupButtonEdit.disabled = false;
+  popupButtonEdit.classList.remove('popup__button_disabled');
+  formElementEdit.classList.remove('popup__input_type_error');
+  openPopup(popupEdit);
 }
 
-infoButton.addEventListener('click', editPopup);
+infoButton.addEventListener('click', () => {
+  editPopup();
+  isValid(formElementEdit, nameInputEdit);
+  isValid(formElementEdit, jobInputEdit);
+});
 
 function handleProfileFormSubmit (evt) {
   evt.preventDefault(); 
@@ -121,10 +143,35 @@ function addCard() {
 }
 buttonPlus.addEventListener('click', addCard);
 
+const escKey = 'Escape';
+const onDocumentKeyUp = (event) => {
+  if (event.key === escKey) {
+    const popupOpened = document.querySelector('.popup_opened');
+    closePopup(popupOpened);
+  }
+}
+
 function openPopup(popup) {
+  if (titleInputCard.value.trim().length === 0 && linkInputCard.value.trim().length === 0) {
+    popupButtonCard.classList.add('popup__button_disabled');
+    popupButtonCard.disabled = true;
+  } else {
+    popupButtonCard.disabled = false;
+  }
   popup.classList.add('popup_opened');
+  document.addEventListener('keyup', onDocumentKeyUp);
 }
 
 function closePopup(popup) {
   popup.classList.remove('popup_opened');
+  document.removeEventListener('keyup', onDocumentKeyUp);
 }
+
+const findPopup = Array.from(document.querySelectorAll('.popup'));
+findPopup.forEach (popup => {
+  popup.addEventListener('mousedown', (evt) => {
+     if (evt.target.classList.contains('popup_opened')) {
+      closePopup(popup);
+    }
+  })
+});
