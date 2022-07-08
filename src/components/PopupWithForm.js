b16cc8fa36476b45109657434a42ff8ee1247df6
Mary Ball <marusaball@yandex.ru>
2022-07-08 18:23:07 +0300

    проектная работа 8(2)

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -5,10 +5,10 @@ export default class PopupWithForm extends Popup {
       super(popupSelector);
       this._submitForm = submitForm;
       this._popupContainer = this._popup.querySelector(".popup__container");
+      this._inputList = this._popupContainer.querySelectorAll('.popup__input');
     }
 
     _getInputValues () {
-      this._inputList = this._popupContainer.querySelectorAll('.popup__input');
       this._formValues = {};
       this._inputList.forEach(input => {
         this._formValues[input.name] = input.value;
